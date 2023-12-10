7ae629edca7347b7c8ee7c36f38236801f550d15
Nurulla Ibadov <ibadnurulla@gmail.com>
2023-12-10 20:09:12 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function checkAnswer() {
+  let answer = document.querySelector(".last");
+  answer.textContent = "1";
+  window.location.href = "end.html";
+}
+
+function checkAnswer2() {
+  let answer = document.querySelector(".last");
+  answer.textContent = "2";
+  window.location.href = "end.html";
+}
+
+function checkAnswer3() {
+  let answer = document.querySelector(".last");
+  answer.textContent = "3";
+  window.location.href = "end.html";
+}
+
+function checkAnswer4() {
+  let answer = document.querySelector(".last");
+  answer.textContent = "4";
+  window.location.href = "end.html";
+}
+
+function checkAnswer5() {
+  let answer = document.querySelector(".last");
+  answer.textContent = "5";
+  window.location.href;
+}
